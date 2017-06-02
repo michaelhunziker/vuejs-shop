9e3de8ad193882c9c20902fcfb1c95b081b91544
Michael Hunziker <michael.hunziker@youengineering.com>
2017-06-02 11:56:47 +0200

    Renamed component.

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>vue.shop - for a better l<span class="infinity">&infin;</span>k!</h1>
+    <h2>sunglasses</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'list',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .infinity {
+    font-size: 1.3em;
+    color: #42b983
+  }
+</style>
